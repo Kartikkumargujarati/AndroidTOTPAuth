74a94ac77ed97f01a8d970698dc4e5672d943417
Kartik Kumar Gujarati <kartikkumar.gujarati@rackspace.com>
2018-02-19 19:30:19 -0600

    New RecyclerView with custom adapter with functionality to add new items to list.

--- app/src/main/java/com/mobile/androidtotpauth/MainActivity.kt
+++ app/src/main/java/com/mobile/androidtotpauth/MainActivity.kt
@@ -2,11 +2,36 @@ package com.mobile.androidtotpauth
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var mAdapter: AccountListAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val _recyclerView: RecyclerView = findViewById(R.id.list_items)
+        _recyclerView.layoutManager = LinearLayoutManager(this)
+        _recyclerView.hasFixedSize()
+        mAdapter = AccountListAdapter(getLists())
+        _recyclerView.adapter = mAdapter
+        add_btn.setOnClickListener { onClickAddBtn() }
+    }
+
+    private fun getLists(): ArrayList<AuthAccount> {
+
+        var lists = ArrayList<AuthAccount>()
+        for (i in 1..10) {
+            lists.add(AuthAccount(i))
+        }
+        return lists;
+    }
+
+    private fun onClickAddBtn() {
+        mAdapter!!.add()
     }
 }
+
